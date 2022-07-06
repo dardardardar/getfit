0d1c0bcd9de1d1cc01b8a9e7e4306061d02e6ca8
rakhafawwaz <61929410+rakhafawwaz@users.noreply.github.com>
2022-07-07 00:41:05 +0700

    page editprofile, my goal, request

--- lib/views/editprofileView.dart
+++ lib/views/editprofileView.dart
@@ -12,6 +12,135 @@ class editprofileView extends StatefulWidget {
 class _editprofileViewState extends State<editprofileView> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: Container(
+          margin: EdgeInsets.only(left: 8),
+          child: Row(
+            children: [
+              Image(
+                image: AssetImage("assets/images/back.png"),
+              ),
+              Column(
+                children: [
+                  Text('Edit Profile'),
+                ],
+              ),
+            ],
+          ),
+        ),
+        backgroundColor: LibColors.primary_color,
+      ),
+      body: Container(
+          margin: EdgeInsets.all(12),
+          child: SingleChildScrollView(
+              child: Column(
+            children: <Widget>[
+              Container(
+                margin: EdgeInsets.only(top: 16),
+                width: 350,
+                height: 50,
+                child: TextField(
+                  textAlign: TextAlign.center,
+                  decoration: InputDecoration(
+                    hintText: "Bambang Atkinson",
+                    border: OutlineInputBorder(),
+                    prefixIcon: Icon(Icons.create),
+                  ),
+                ),
+              ),
+              Container(
+                margin: EdgeInsets.only(top: 16),
+                width: 350,
+                height: 50,
+                child: TextField(
+                  textAlign: TextAlign.center,
+                  decoration: InputDecoration(
+                    hintText: "170 CM",
+                    border: OutlineInputBorder(),
+                    prefixIcon: Icon(Icons.create),
+                  ),
+                ),
+              ),
+              Container(
+                margin: EdgeInsets.only(top: 16),
+                width: 350,
+                height: 50,
+                child: TextField(
+                  textAlign: TextAlign.center,
+                  decoration: InputDecoration(
+                    hintText: "85 Kg",
+                    border: OutlineInputBorder(),
+                    prefixIcon: Icon(Icons.create),
+                  ),
+                ),
+              ),
+              Container(
+                margin: EdgeInsets.only(top: 16),
+                width: 350,
+                height: 50,
+                child: TextField(
+                  textAlign: TextAlign.center,
+                  decoration: InputDecoration(
+                    hintText: "01/01/1991",
+                    border: OutlineInputBorder(),
+                    prefixIcon: Icon(Icons.create),
+                  ),
+                ),
+              ),
+              Container(
+                margin: EdgeInsets.only(top: 16),
+                width: 350,
+                height: 50,
+                child: TextField(
+                  textAlign: TextAlign.center,
+                  decoration: InputDecoration(
+                    hintText: "Male",
+                    border: OutlineInputBorder(),
+                    prefixIcon: Icon(Icons.create),
+                  ),
+                ),
+              ),
+              // editprofileCard(),
+            ],
+          ))),
+    );
   }
+}
+
+Widget editprofileCard() {
+  return ClipRRect(
+      borderRadius: BorderRadius.circular(12),
+      child: Container(
+        child: Row(
+          children: <Widget>[
+            Container(
+              child: Column(
+                children: [
+                  Container(
+                    width: 170,
+                    child: Text(
+                      'Bambang Atkinson',
+                      textAlign: TextAlign.start,
+                      style: TextStyle(
+                        color: Color.fromARGB(255, 0, 0, 0),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Expanded(
+              child: Column(
+                children: [
+                  Image(
+                      image: AssetImage("assets/images/editicon.png"),
+                      fit: BoxFit.cover),
+                ],
+              ),
+            ),
+          ],
+        ),
+        // color: LibColors.second_color,
+      ));
 }
---
+++ lib/views/mygoalView.dart
@@ -0,0 +1,168 @@
+import 'dart:ui';
+import 'package:flutter/material.dart';
+import 'package:getfit/widgets/colors.dart';
+import '../widgets/text_widgets.dart';
+
+class MygoalView extends StatefulWidget {
+  const MygoalView({Key? key}) : super(key: key);
+
+  @override
+  State<MygoalView> createState() => _MygoalViewState();
+}
+
+class _MygoalViewState extends State<MygoalView> {
+  PageController p = PageController();
+  int _goalCategory = 0;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Container(
+          margin: EdgeInsets.only(left: 8),
+          child: Row(
+            children: [
+              Image(
+                image: AssetImage("assets/images/back.png"),
+              ),
+              Column(
+                children: [
+                  Text('My Goal'),
+                ],
+              ),
+            ],
+          ),
+        ),
+        backgroundColor: LibColors.primary_color,
+      ),
+      body: Container(
+        padding: EdgeInsets.all(12),
+        // color: LibColors.primary_color,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Align(
+                heightFactor: 2,
+                child: Text(
+                  "Select Your Goal",
+                  style: TextStyle(color: Colors.black),
+                )),
+            Container(
+              margin: EdgeInsets.all(16),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  Align(
+                      alignment: Alignment.center,
+                      child: Column(
+                        children: [
+                          SizedBox(
+                            height: 16,
+                          ),
+                          MaterialButton(
+                              onPressed: () {
+                                setState(() {
+                                  _goalCategory = 0;
+                                });
+                                p.nextPage(
+                                    duration: Duration(milliseconds: 500),
+                                    curve: Curves.easeIn);
+                              },
+                              minWidth: 360,
+                              height: 80,
+                              color: LibColors.primary_color,
+                              child: Text(
+                                "DIET",
+                                style: const TextStyle(
+                                  fontSize: 16,
+                                  color: Color.fromARGB(255, 255, 255, 255),
+                                ),
+                                textAlign: TextAlign.center,
+                              )),
+                          SizedBox(
+                            height: 16,
+                          ),
+                          MaterialButton(
+                              onPressed: () {
+                                setState(() {
+                                  _goalCategory = 1;
+                                });
+                                p.nextPage(
+                                    duration: Duration(milliseconds: 500),
+                                    curve: Curves.easeIn);
+                              },
+                              minWidth: 360,
+                              height: 80,
+                              color: LibColors.primary_color,
+                              child: Text(
+                                "MAINTAIN WEIGHT",
+                                style: const TextStyle(
+                                  fontSize: 16,
+                                  color: Color.fromARGB(255, 255, 255, 255),
+                                ),
+                                textAlign: TextAlign.center,
+                              )),
+                          SizedBox(
+                            height: 16,
+                          ),
+                          MaterialButton(
+                              onPressed: () {
+                                setState(() {
+                                  _goalCategory = 2;
+                                });
+                                p.nextPage(
+                                    duration: Duration(milliseconds: 500),
+                                    curve: Curves.easeIn);
+                              },
+                              minWidth: 360,
+                              height: 80,
+                              color: LibColors.primary_color,
+                              child: Text(
+                                "LOSE WEIGHT",
+                                style: const TextStyle(
+                                  fontSize: 16,
+                                  color: Color.fromARGB(255, 255, 255, 255),
+                                ),
+                                textAlign: TextAlign.center,
+                              )),
+                          SizedBox(
+                            height: 90,
+                          ),
+                        ],
+                      ))
+                ],
+              ),
+            ),
+            // Container(
+            //   margin: EdgeInsets.all(8),
+            //   child: Column(
+            //     mainAxisAlignment: MainAxisAlignment.end,
+            //     children: [
+            //       Align(
+            //           alignment: Alignment.center,
+            //           child: Column(
+            //             children: [
+            //               MaterialButton(
+            //                   onPressed: () {
+            //                     p.previousPage(
+            //                         duration: Duration(milliseconds: 500),
+            //                         curve: Curves.easeIn);
+            //                   },
+            //                   child: Text(
+            //                     "Back",
+            //                     style: const TextStyle(
+            //                       fontSize: 16,
+            //                       color: LibColors.color_white,
+            //                     ),
+            //                     textAlign: TextAlign.center,
+            //                   ))
+            //             ],
+            //           ))
+            //     ],
+            //   ),
+            // )
+          ],
+        ),
+      ),
+    );
+  }
+}

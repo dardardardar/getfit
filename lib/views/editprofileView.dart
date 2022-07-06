aaf900db48bea92015b2d3834246b4700e982500
rakhafawwaz <61929410+rakhafawwaz@users.noreply.github.com>
2022-07-06 22:02:32 +0700

    editprofilepage

---
+++ lib/views/editprofileView.dart
@@ -0,0 +1,17 @@
+import 'dart:ui';
+import 'package:flutter/material.dart';
+import 'package:getfit/widgets/colors.dart';
+
+class editprofileView extends StatefulWidget {
+  const editprofileView({Key? key}) : super(key: key);
+
+  @override
+  State<editprofileView> createState() => _editprofileViewState();
+}
+
+class _editprofileViewState extends State<editprofileView> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
